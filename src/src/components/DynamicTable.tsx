aeba44179609ffa8de7225b7c18ab4a0b697fc8c
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2022-10-11 21:48:52 -0500

    Add files via upload

---
+++ src/src/components/DynamicTable.tsx
@@ -0,0 +1,82 @@
+import { Column, useTable } from "react-table";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import React, { useEffect, useState } from "react";
+
+type SimpleObject = { [key: string]: string | number | React.ReactNode | null };
+
+interface DynamicTableProps {
+  columns: Column<SimpleObject>[];
+  data: SimpleObject[];
+  filter?: string | null;
+}
+
+function DynamicTable({ columns, data }: DynamicTableProps) {
+  const [active, setActive] = useState(false);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+  useEffect(() => {
+    setActive(true);
+  }, []);
+
+  return (
+    <TableContainer component={Paper} className="shadow-none " >
+      <Table {...getTableProps()}>
+        <TableHead className="shadow-sm ">
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()} >
+              {headerGroup.headers.map((column) => (
+                <TableCell
+                  className={`bg-white border-white fs-6 text-primary text-opacity-75 opacity${
+                    active ? "active" : ""
+                  } transitionUp ${active ? "active" : ""}`}
+                >
+                  <strong>
+                  <u><big className="ms-5 ">{column.render("Header")}</big></u>  
+                  </strong>
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody
+          {...getTableBodyProps()}
+          className={` opacity${active ? "active" : ""} transitionLeft ${
+            active ? "active" : ""
+          }`}
+        >
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <TableRow key={row.id}>
+                {row.cells.map((cell) => (
+                  <TableCell
+                    {...cell.getCellProps()}
+                    className={`bg-gray1 border-4 border-white rounded-4`}
+                  >
+                    <strong>
+                      <a
+                        className="text-black fs-7 ms-7"
+                        style={{ textDecoration: "none" }}
+                      >
+                        {" "}
+                        {cell.render("Cell")}
+                      </a>
+                    </strong>
+                  </TableCell>
+                ))}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default DynamicTable;
---
+++ src/src/screens/Docente.tsx
@@ -0,0 +1,110 @@
+import DynamicTable from "../components/DynamicTable";
+import useWindowSize from "../hooks/useWindowSize";
+import { useMemo } from "react";
+import { useGetTeachersQuery } from "../generated/graphql";
+import { useEffect, useState } from "react";
+import SideBarWithText from "../components/SideBarWithText";
+import edit from "../assets/01editar.png";
+import delet from "../assets/01eliminar.png";
+
+const columns = [
+  {
+    Header: "Apellido" ,
+    accessor: "lastName",
+  },
+  {
+    Header: "Nombre",
+    accessor: "name",
+  },
+  {
+    Header: "Título",
+    accessor: "degree",
+  },
+  {
+    Header: "Editar",
+    accessor: "editar",
+  },
+  {
+    Header: "Borrar",
+    accessor: "borrar",
+  },
+];
+
+function Docentes() {
+  const [active, setActive] = useState(false);
+  const { data, loading, error } = useGetTeachersQuery();
+  useEffect(() => {
+    setActive(true);
+  }, []);
+  const processedTeachers = useMemo(() => {
+    if (!data?.teachers) return [];
+    return data.teachers.map((teachers, index) => ({
+      name: teachers?.name ?? "",
+      lastName: teachers?.lastName ?? "",
+      degree: teachers?.degree ?? "",
+      editar: <button className="border-0"><img className={`h-13 w-15`} src={edit}/></button>,
+      borrar: <button className="border-0 " ><img className={`h-8 w-10`} src={delet}/></button>,
+    }));
+  }, [data]);
+  const windowSize = useWindowSize();
+
+  return (
+    <SideBarWithText location = "Funcionarios   > Docentes">
+      <div className="w-100 h-100 btl bg-gray1">
+        <div
+          className={`ps-10 mw-100 h-full row ${
+            (windowSize.width ?? 0) >= 1200 ? "mt-16" : ""
+          }`}
+        >
+          <div className="d-flex col-11 ms-20 fw-bold">
+            <strong className="fs-4 ms-10 me-81 pe-20">Lista de Docentes</strong>
+            <button
+              type="button"
+              className="btn bg-blue3 btn-primary ms-81 w-64 mb-0 pb-0 h-10 btl btr "
+            >
+              <a>
+                <h4 className="text-white fs-5">+ Nuevo Docente</h4>
+              </a>
+            </button>
+          </div>
+          <div className="col-lg-11 col-md-10 mx-auto bg-white rounded border border-2 shadow rounded-5">
+            <form className="d-flex justify-content-between my-4" role="search">
+              <h5
+                className={`col-6 opacity${
+                  active ? "active" : ""
+                } transitionRight ${active ? "active" : ""}`}
+              >
+                <input
+                  className="col-12 bg-gray2 rounded-5 border-0 p-3 ps-3 fs-5 my-2 ms-15"
+                  type="search"
+                  placeholder="Buscar Docente"
+                  aria-label="Search"
+                />
+              </h5>
+              <select
+                className={`bg-gray2 text-gray3 rounded-5 border-0 p-2 fs-5 my-2 me-15 opacity${
+                  active ? "active" : ""
+                } transitionDown ${active ? "active" : ""}`}
+              >
+                <option>Filtrar por</option>
+                <option>Nombre</option>
+                <option>Apellido</option>
+              </select>
+            </form>
+            {error && <div>¡Ocurrio un error!</div>}
+            {data?.teachers && !loading && (
+              <div
+                className="d-flex border-white py-4"
+                style={{ height: "32rem" }}
+              >
+                <DynamicTable columns={columns} data={processedTeachers} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </SideBarWithText>
+  );
+}
+
+export default Docentes;
